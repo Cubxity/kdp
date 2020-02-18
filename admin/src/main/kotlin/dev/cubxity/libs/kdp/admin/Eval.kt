e762e36e8baaaefab94a65f30bb79ab7b91420a8
Koding <kodingdev@pm.me>
2020-02-19 09:34:46 +1100

    Fixed ignore quotes reference

--- admin/src/main/kotlin/dev/cubxity/libs/kdp/admin/Eval.kt
+++ admin/src/main/kotlin/dev/cubxity/libs/kdp/admin/Eval.kt
@@ -19,6 +19,7 @@
 package dev.cubxity.libs.kdp.admin
 
 import dev.cubxity.libs.kdp.perms.botAdmin
+import dev.cubxity.libs.kdp.processing.ignoreQuotes
 import dev.cubxity.libs.kdp.utils.embed.codeBlock
 import dev.cubxity.libs.kdp.utils.embed.embed
 import org.apache.commons.lang3.exception.ExceptionUtils
