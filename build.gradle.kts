44668202530bcce06e52f2152160e2ea1935e367
Cubxity <contact@cubxity.dev>
2020-10-27 17:02:02 +0100

    Added core module, buildscript customization

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,24 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
     kotlin("jvm") version "1.4.10" apply false
 }
 
-group = "dev.cubxity.kdp"
-version = "2.0.0"
-
-subprojects {
-    apply(plugin = "kotlin")
+allprojects {
+    group = "dev.cubxity.kdp"
+    version = "2.0.0"
 
     repositories {
         mavenCentral()
+        jcenter()
+    }
+}
+
+subprojects {
+    tasks.withType<KotlinCompile> {
+        kotlinOptions {
+            jvmTarget = "1.8"
+            freeCompilerArgs = listOf("-Xinline-classes")
+        }
     }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,2 @@
 rootProject.name = "kdp"
+include(":core")
